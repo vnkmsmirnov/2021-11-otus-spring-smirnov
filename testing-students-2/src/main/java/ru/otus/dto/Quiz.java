82e0a2ec70b98afe3177bd1c45ab9ac38bd20d40
m.smirnov <vnk.m.smirnov@gmail.com>
2021-11-29 15:10:02 +0300

    lesson-2 fix

--- testing-students-2/src/main/java/ru/otus/dto/Quiz.java
+++ testing-students-2/src/main/java/ru/otus/dto/Quiz.java
@@ -1,14 +1,14 @@
 package ru.otus.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Data
-@SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Quiz {
